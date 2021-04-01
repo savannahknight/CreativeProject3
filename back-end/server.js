e3c4b7b7345270e1a9076b722b136688b4d6b27b
Savannah Knight <savannahruthknight.com>
2021-04-01 13:38:01 -0600

    sav commit

--- back-end/server.js
+++ back-end/server.js
@@ -2,13 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
-// parse application/x-www-form-urlencoded
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+const mongoose = require('mongoose');
 
-// parse application/json
-app.use(bodyParser.json());
+// connect to the database
+mongoose.connect('mongodb://localhost:27017/playlist', {
+  useNewUrlParser: true
+});
+const multer = require('multer')
+const upload = multer({
+  dest: '../front-end/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+
+const itemSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  path: String,
+  profile: String,
+});
+const Item = mongoose.model('Item', itemSchema);
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
